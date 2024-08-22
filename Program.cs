6e73f7e94bdf93ea5f6d32581c8dcbf3b69eca44
hp <hp@DESKTOP-4BSJICM>
2024-08-22 01:16:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+
+public class program
+{
+    public static void Main()
+    {
+        //      Console.WriteLine("Enter Your First Name");
+        //      string Fname= Console.ReadLine();
+
+        //      Console.WriteLine("Enter Your Middle Name");
+        //string Mname = Console.ReadLine();
+
+        //      Console.WriteLine("Enter Your Last Name");
+        //      string Lname = Console.ReadLine();
+
+        //string Fullname = Fname + " " + Mname + " " + Lname;
+        //      Console.WriteLine(Fullname);
+
+        //Calculate Length Of The String
+        //Console.WriteLine("Enter Any Name");
+        //var name = Console.ReadLine();
+
+        //int count = 0;
+
+        //foreach (var item in name)
+        //{
+        //    count++;
+        //}
+        //Console.WriteLine("Calculate Length Of The String= "+count);
+
+        //Calculate Length Of The Vowels
+        Console.WriteLine("Enter Any Name");
+        var Vowels = Console.ReadLine();
+
+        int VowelsCount = 0;
+
+        foreach(var item in Vowels)
+        {
+            if(item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
+            {
+                VowelsCount++;
+            }
+        }
+        Console.WriteLine("length Of The Vowels= "+VowelsCount);
+    }
+}

c8761fa14905edcbf0f879289e6905816b265bf3
Shrikant Pawar <shrikantpawar703@gmail.com>
2024-08-23 00:51:28 -0700

    Function Declaration And Defination

--- Program.cs
+++ Program.cs
@@ -1,46 +1,134 @@
 ﻿using Microsoft.VisualBasic;
+using System.Data;
 
 public class program
 {
     public static void Main()
     {
-        //      Console.WriteLine("Enter Your First Name");
-        //      string Fname= Console.ReadLine();
+        // Function Call
 
-        //      Console.WriteLine("Enter Your Middle Name");
-        //string Mname = Console.ReadLine();
+  //      ShowFullName(); 
 
-        //      Console.WriteLine("Enter Your Last Name");
-        //      string Lname = Console.ReadLine();
+  //      CalculString(); 
 
-        //string Fullname = Fname + " " + Mname + " " + Lname;
-        //      Console.WriteLine(Fullname);
+  //      Vowels(); 
 
-        //Calculate Length Of The String
-        //Console.WriteLine("Enter Any Name");
-        //var name = Console.ReadLine();
+		//Spaces(); 
 
-        //int count = 0;
+		//Consonent(); 
 
-        //foreach (var item in name)
-        //{
-        //    count++;
-        //}
-        //Console.WriteLine("Calculate Length Of The String= "+count);
+        CalculWord();
 
-        //Calculate Length Of The Vowels
+	}
+
+    // Decalaration And Defination
+
+    static void ShowFullName()
+	{      
+        //  Show Full Name By User
+
+        Console.WriteLine("Enter Your First Name");
+        string Fname = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Middle Name");
+        string Mname = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Last Name");
+        string Lname = Console.ReadLine();
+
+        string Fullname = Fname + " " + Mname + " " + Lname;
+        Console.WriteLine(Fullname);
+    }
+
+    static void CalculString()
+    {
+       // Calculate Length Of The String(Calculate Character With Space)
+
+
+        Console.WriteLine("Enter Any Name");
+        var name = Console.ReadLine();
+
+        int count = 0;
+
+        foreach (var item in name)
+        {
+            count++;
+        }
+        Console.WriteLine("Calculate Length Of The String= " + count);
+    }
+
+   static void Vowels()
+    {
+        // Calculate Length Of The Vowels
+       
         Console.WriteLine("Enter Any Name");
         var Vowels = Console.ReadLine();
 
         int VowelsCount = 0;
 
-        foreach(var item in Vowels)
+        foreach (var item in Vowels)
         {
-            if(item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
             {
                 VowelsCount++;
             }
         }
-        Console.WriteLine("length Of The Vowels= "+VowelsCount);
+        Console.WriteLine("length Of The Vowels= " + VowelsCount);
+    }
+
+    static void Spaces()
+    {
+        //Calculate Length Of The Spaces
+        Console.WriteLine("Enter Any Name");
+        var Space = Console.ReadLine();
+
+        int SpaceCount = 0;
+
+        foreach (var item in Space)
+        {
+            if (item == ' ')
+            {
+                SpaceCount++;
+            }
+        }
+        Console.WriteLine("Number Of Spaces " + SpaceCount);
+    }
+
+    static void Consonent()
+    {
+		// Calculate How Many Consonent
+
+		Console.WriteLine("Enter Any Name");
+		var Consonent = Console.ReadLine();
+
+		int ConsonentCount = 0;
+
+		foreach (var item in Consonent)
+		{
+			if (!(item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' || item == ' '))
+			{
+				ConsonentCount++;
+			}
+		}
+		Console.WriteLine("Number Of Consonent " + ConsonentCount);
+	}
+
+    static void CalculWord()
+    {
+        // Calculate Word By Using Spaces
+
+        Console.WriteLine("Enter Any Name"); 
+        var CalculWord = Console.ReadLine();
+
+        int CalculWordCount = 0;
+
+        foreach(var item in CalculWord)
+        {
+            if (item == ' ')
+            {
+                CalculWordCount++;
+            }
+        }
+        Console.WriteLine("Number Of The Word = " + (CalculWordCount + 1));
     }
 }
